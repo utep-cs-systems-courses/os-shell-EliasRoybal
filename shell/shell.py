5a879d305bc26dc72c38a8d478e47767079e337d
EliasRoybal <eroybal@miners.utep.edu>
2021-02-14 21:53:42 -0700

    Changing directories now works. Also fixed issue where entire command would turn lower case instead of just checking for certain lower case commands (Example: exit and cd)

--- shell/shell.py
+++ shell/shell.py
@@ -2,20 +2,32 @@ import os, sys, re
 
 def initialPrompt():
     while 1:
-        prompt = os.write(1, ("$ ").encode())
+        currDirectoryPrompt = os.getcwd() +" $ " #includes current directory with '$'
+        
+        prompt = os.write(1, (currDirectoryPrompt).encode())
 
         if 'PS1' in os.environ:
             prompt = os.environ['PS1']
         
         args = os.read(0, 10000)
 
-        args = args.decode().lower().strip("\n")
+        args = args.decode().strip("\n").split()
 
         commandHandler(args)
 
 def commandHandler(args):
-    if args  == 'exit':
+    currDirectory = os.getcwd()
+    
+    if args[0].lower() == 'exit':
         sys.exit(0)
+
+    elif args[0].lower() == 'cd':
+        try:
+            os.chdir(args[1])
+        except FileNotFoundError:
+            print("No such directory found")
+        except IndexError: # no argument after 'cd'
+            os.chdir(currDirectory)
         
     else:
         print(args)
