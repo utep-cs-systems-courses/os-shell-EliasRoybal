26518be70fb03af54f4b7786d607033a3800c39f
EliasRoybal <eroybal@miners.utep.edu>
2021-02-14 20:49:53 -0700

    Created new method to take in argument from user input. Also added environment variables

--- shell/shell.py
+++ shell/shell.py
@@ -2,14 +2,22 @@ import os, sys, re
 
 def initialPrompt():
     while 1:
-        os.write(1, ("$ ").encode())
+        prompt = os.write(1, ("$ ").encode())
+
+        if 'PS1' in os.environ:
+            prompt = os.environ['PS1']
         
         args = os.read(0, 10000)
 
-        if args.decode().lower().strip("\n") == 'exit':
-            sys.exit(0)
-            
-        else:
-            print("input working")
+        args = args.decode().lower().strip("\n")
+
+        commandHandler(args)
+
+def commandHandler(args):
+    if args  == 'exit':
+        sys.exit(0)
+        
+    else:
+        print(args)
 
 initialPrompt()
