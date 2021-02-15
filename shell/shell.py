484f840bdaeadc2fd1e47b53f070a81282c4af51
EliasRoybal <eroybal@miners.utep.edu>
2021-02-14 23:54:23 -0700

    Added fork and it now accepts other commands beside 'cd'. Bug where prompt prints out twice after first command

--- shell/shell.py
+++ shell/shell.py
@@ -1,7 +1,9 @@
 import os, sys, re
 
+pid = os.getpid()
+
 def initialPrompt():
-    while 1:
+    while True:
         currDirectoryPrompt = os.getcwd() +" $ " #includes current directory with '$'
         
         prompt = os.write(1, (currDirectoryPrompt).encode())
@@ -16,20 +18,36 @@ def initialPrompt():
         commandHandler(args)
 
 def commandHandler(args):
-    currDirectory = os.getcwd()
-    
-    if args[0].lower() == 'exit':
+
+    if len(args) == 0:
+        pass
+   
+    elif args[0].lower() == 'exit':
         sys.exit(0)
 
     elif args[0].lower() == 'cd':
         try:
             os.chdir(args[1])
         except FileNotFoundError:
-            print("No such directory found")
+            print("No such directory founds")
         except IndexError: # no argument after 'cd'
-            os.chdir(currDirectory)
+            pass
         
     else:
-        print(args)
+        rc = os.fork()
 
+        if rc < 0: #fork failed
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            return
+        elif rc == 0:  # child
+            os.write(1,"\n".encode())
+            for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(program, args, os.environ) # try to exec program
+                except FileNotFoundError:             # ...expected
+                    pass                              # ...fail quietly
+                except Exception:
+                    pass
+            os.write(1, "Command not found\n".encode())
 initialPrompt()
